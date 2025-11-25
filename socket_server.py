1a92dc97d5e86537c3222edff50ce7407b1212ca
IasminaPaguE <iasmina-andra.pagu@endava.com>
2025-11-25 14:43:23 +0200

    Added the multi-threading functionality on the server, in order to receive and handle multiple connections from multiple clients on the same time

--- socket_server.py
+++ socket_server.py
@@ -1,27 +1,40 @@
 import socket
+import threading
+
+
+def handle_client(conn, address):
+    print("Connection from " + str(address))
+    while True:
+        try:
+            data = conn.recv(1024).decode()
+            if not data:
+                print(f"Connection ended {address}")
+                break
+            print(f"From {address}: {data}")
+            response = input(" -> ")
+            conn.send(response.encode())
+        except ConnectionResetError:
+            print(f"Error Connection reset by {address}")
+            break
+    conn.close()
 
 
 def server_program():
-    host = socket.gethostname() #obtain host adress
-    port = 5000 
+    host = socket.gethostname()
+    port = 5000
 
-    server_socket = socket.socket()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
+    server_socket.listen(5)
 
-    server_socket.listen(2)
-    conn, address = server_socket.accept()
+    print(f"Server is listening on {host}:{port}")
 
-    print("Connection from " + str(address))
     while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        print("from connected user" + str(data))
-        data = input(' -> ')
+        conn, address = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, address))
+        thread.start()
+        print(f"Active connections {threading.active_count() - 1}")
 
-        conn.send(data.encode())
-
-    conn.close()
 
 if __name__ == "__main__":
     server_program()
